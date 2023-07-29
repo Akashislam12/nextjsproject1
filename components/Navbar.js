15ff3208c5a174b145b2262a93bffda378d1aaa9
Akash islam <akashkhan3588@gmail.com>
2023-07-29 12:10:42 +0600

    dcommit

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='mb-3'>
       <nav className='flex'>
-        <h1 className='inline ml-20 rounded-sm text-xl red bg-gray-400 p-1 '>AKASH'Profile</h1>
+        <h1 className='inline ml-20 rounded-sm text-xl red bg-gray-400 p-1 '>AKASH`Profile</h1>
       <ul className='flex text-center justify-center m-auto py-3 '>
         <Link className='mx-3' href="/"><li>Home</li></Link>
         <Link className='mx-3' href="/about"><li>About</li></Link>
